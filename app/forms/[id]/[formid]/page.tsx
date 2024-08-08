8de10f08d8b4ed658bbcf8b926e73f4bb66c489e
charlesrzf <charlesrf@gmail.com>
2024-08-08 08:31:03 -0300

    corrige id

--- app/forms/[id]/[formid]/page.tsx
+++ app/forms/[id]/[formid]/page.tsx
@@ -61,9 +61,13 @@ const FormPageUpdate = () => {
           setValue(key, dayjs(value as string).format("YYYY-MM-DD"));
         } else if (typeof value === "object" && value !== null) {
           if (key === "surveycoordinate") {
-            setValue("surveyCoordinate", value.id);
+            if ('id' in value) {
+              setValue("surveyCoordinate", value.id);
+            }
           } else {
-            setValue(key, value.id);
+            if ('id' in value) {
+              setValue(key, value.id);
+            }
           }
         } else {
           setValue(key, value);
